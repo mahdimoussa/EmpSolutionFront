f43111a46486f1fd9ca2cc8ad0135a5625ff76e3
mahdi moussa <mahdi.m.moussa@gmail.com>
2021-02-20 04:00:24 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from "./dashboard";
+import Login from './Login';
+import Register from './Register';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    localStorage.setItem("loggedIn","False")
+  }
+  render(){
+    return (
+      <Router>
+        <div>
+          
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+          <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/">
+              <Register />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+ 
 }
-
-export default App;
+export default App
---
+++ src/dashboard.js
@@ -0,0 +1,83 @@
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import Badge from 'react-bootstrap/Badge';
+import {
+    Link
+} from "react-router-dom";
+import './App.scss';
+import PaginationTableComponent from './PaginationTableComponent.js';
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url:"http://localhost:8000/api/",     
+            averagelastday:0,
+            averagelastweek:0,
+            averagelastmonth:0,
+            averagelastthreemonths:0,
+            averagelastyear:0,
+            data:[]
+        }
+ 
+    
+        
+
+    }
+    componentDidMount()
+    {
+        this.getAvg("averagelastday")
+        this.getAvg("averagelastweek")
+        this.getAvg("averagelastmonth")
+        this.getAvg("averagelastthreemonths")
+        this.getAvg("averagelastyear")
+        this.getCustomers(20)
+
+    }
+    getAvg = (key)=>{
+            axios.get(this.state.url + "customers/" + key).then((response)=>{
+                this.setState({key:response})
+            });
+      }
+    getCustomers(pageSize){
+        axios.post(this.state.url + "customers", {customersPerPage:pageSize}).then((response)=>{
+            console.log(response)
+            this.setState({data:response.data.data})
+        });
+    }
+
+render() {
+    return (
+<div style={{backgroundColor:'#adb5bd'}} >
+ <h2>Admin Panel</h2> 
+ <nav className="btn btn-danger" style={{marginLeft:'90%',marginTop:'-37px'}}>
+       <li>
+            <Link style={{color:'#FFF'}} to="/">Logout</Link>
+        </li> 
+</nav>
+
+ <div>
+  <h3>
+  Last 24 Hours Registrations <Badge variant="secondary">{this.state.averagelastday}</Badge>
+  </h3>
+  <h3>
+  Last Week Registrations <Badge variant="secondary">{this.state.averagelastweek}</Badge>
+  </h3>
+  <h3>
+  Last Month Registrations <Badge variant="secondary">{this.state.averagelastmonth}</Badge>
+  </h3>
+  <h3>
+  Last 3 Months Registrations <Badge variant="secondary">{this.state.averagelastthreemonths}</Badge>
+  </h3>
+  <h3>
+  Last Year Registrations <Badge variant="secondary">{this.state.averagelastyear}</Badge>
+  </h3>
+
+</div>
+<PaginationTableComponent data={this.state.data}/>
+</div>
+    );
+ }
+}
+export default Dashboard;
